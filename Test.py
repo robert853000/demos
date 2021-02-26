6947a9d9cc94a868183041393a3d9a2c1996404f
Robert Str <robert705007@gmail.com>
2021-02-26 23:55:17 +0100

    Add python code

--- Test.py
+++ Test.py
@@ -1,19 +1,19 @@
 (2 + 3 * 6 / 2) * 1
-print ("Programs tend to have bugs")
+print("Programs tend to have bugs")
 
 name = "Interpretive lang -> -> "
 
-age = 36 
+age = 36
 
-print (age)
+print(age)
 
-print (type(age))
+print(type(age))
 
-print ("Someone programmed this code")
+print("Someone programmed this code")
 
 2 + 3
 
-2 * 3 
+2 * 3
 
 2 ** 3
 
@@ -31,16 +31,10 @@ print ("Someone programmed this code")
 
 name = input("Whats's your name ? ")
 if name == "python":
-    print ("That's good to hear !")
-    print("line2")
-    print("line3")
+    print("That's good to hear !")
+    print("Line 2")
+    print("Line 3")
 else:
-    print ( "That makes me sad!" )
+    print("That makes me sad!")
 
-# Include comments 
-
-
-
-
-
-
+# Include comments
