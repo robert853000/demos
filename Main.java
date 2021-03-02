d9ce1026510a997e656b5582ecf0af34ca0854cd
Robert Str <robert705007@gmail.com>
2021-03-02 10:36:44 +0100

    Add Java

---
+++ Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Compile to java bytecode");
+        System.out.println("javac main.java");
+        System.out.println("swimming ...");
+        System.out.println("1 2 3 4 ");
+
+
+
+
+    }
+
+}
