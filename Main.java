0cb100c85a0fb51019d173c9bf0cb998121c5d53
Robert Str <robert705007@gmail.com>
2021-03-02 12:26:39 +0100

    Add Java code

--- Main.java
+++ Main.java
@@ -8,10 +8,8 @@ public class Main {
         System.out.println("javac main.java");
         System.out.println("swimming ...");
         System.out.println("1 2 3 4 5");
-
         System.out.println("Getting started with Java");
         System.out.println("--------------------------------------");
-
 
         /* Primitive types */
         int num = 1;
@@ -19,23 +17,19 @@ public class Main {
         char oneChar = 'K';
         boolean isTrue= false;
 
-
         System.out.println(num);
 
-
         /* Reference */
-
         String message = "The quick brown fox jumps over the lazy dog.";
 
         /* End */
-
-
 
         System.out.println(message.charAt(
                 1
         ));
 
         System.out.println("Type your message below" );
+
         Scanner input = new Scanner(System.in);
         String message2= input.nextLine();
 
@@ -44,6 +38,7 @@ public class Main {
         if (message2.equals("Java")){
             System.out.println("You win new car");
             System.out.println("Type number");
+
             int favoriteNumber = input.nextInt();
             if (favoriteNumber < 5){
                 System.out.println("Your inputted num:" + favoriteNumber);
@@ -52,23 +47,25 @@ public class Main {
             System.out.println ("If you had written Java, you would win new car");
         }
 
-
         boolean isOnRepeat =true;
         int i = 0;
         while (isOnRepeat){
             System.out.println(message.charAt(
                   i
             ));
-
             i = i + 1;
             if (i >= message.length()){
                 isOnRepeat = false;
             }
         }
 
-
-
-
+        announceTeaTime();
     }
 
+    public static void announceTeaTime(){
+        System.out.println("Waiting for tea ...");
+        Scanner input = new Scanner(System.in);
+        input.next();
+        System.out.println("Awesome... ");
+    }
 }
