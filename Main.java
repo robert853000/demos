f635ec8836104455be0d67e062174d2a9d970b49
Robert Str <robert705007@gmail.com>
2021-03-02 11:26:13 +0100

    Add Java code

--- Main.java
+++ Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -11,8 +13,33 @@ public class Main {
         System.out.println("--------------------------------------");
 
 
+        /* Primitive */
+        int num = 1;
+        double weight = 3.2;
+        char oneChar = 'K';
+        boolean isTrue= false;
 
 
+        System.out.println(num);
+
+
+        /* Reference */
+
+        String message = "The quick brown fox jumps over the lazy dog.";
+
+        /* End */
+
+
+
+        System.out.println(message.charAt(
+                1
+        ));
+
+        System.out.println("Type your message below" );
+        Scanner input = new Scanner(System.in);
+        String message2= input.nextLine();
+
+        System.out.println("You wrote:" + message2);
 
 
 
