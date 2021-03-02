8f9dfb9b5272c982de22ba96cd36291d67d4335f
Robert Str <robert705007@gmail.com>
2021-03-02 10:43:01 +0100

    Add Java code

--- Main.java
+++ Main.java
@@ -5,7 +5,15 @@ public class Main {
         System.out.println("Compile to java bytecode");
         System.out.println("javac main.java");
         System.out.println("swimming ...");
-        System.out.println("1 2 3 4 ");
+        System.out.println("1 2 3 4 5");
+
+        System.out.println("Getting started with Java");
+        System.out.println("--------------------------------------");
+
+
+
+
+
 
 
 
